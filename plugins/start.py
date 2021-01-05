7638f61303262a22519d540a246b4e1c6cec974a
Syn <38806740+synuser@users.noreply.github.com>
2021-01-05 13:10:32 +0000

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Bots Updates Channel", url="https://t.me/Discovery_Updates")],
+        [InlineKeyboardButton("Support", url="https://telegram.me/share/url?url=**Syn%20-%20YouTube%20Uploader%20**%20%0A%0AA%20Bot%20Which%20Can%20Download%20Videos%20from%20YouTube%20-%20@SynYoutubebot")],
         [InlineKeyboardButton(
-            "Support Group", url="https://t.me/linux_repo")]
+            "Report Bugs", url="https://t.me/synuser")]
     ])
-    welcomed = f"Hi, <b>{message.from_user.first_name}</b>.\nThis is <b>YouTube Uploader Bot.</b>\n\nBy @Discovery_Updates \nDeveloper: @AbirHasan2005"
+    welcomed = f"Hi, <b>{message.from_user.first_name}</b>.\nThis is <b>YouTube Uploader Bot.</b>\n\nfor more details /help\nDeveloper: @Synuser"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
